c23ffd6a8c67d6e93abdd264806caef7fd4f9ab1
Stephen Jacob <stephen.a.jacob@gmail.com>
2015-11-19 16:31:15 +0000

    Accommodation References

---
+++ project/Athlone_Tourist_App/app/src/main/java/ie/ait/touristapp/location/Accommodation.java
@@ -0,0 +1,16 @@
+
+package ie.ait.touristapp.location;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ie.ait.touristapp.R;
+
+public class Accommodation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accommodation);
+    }
+}
