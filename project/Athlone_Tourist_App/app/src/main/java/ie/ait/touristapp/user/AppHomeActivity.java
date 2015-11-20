dca4569ac8f895896e3adc403e6f8ac3dbec8d82
bkenny00 <brendankenny00@gmail.com>
2015-11-20 13:03:40 +0000

    main menu modified, music activity added Brendan

--- project/Athlone_Tourist_App/app/src/main/java/ie/ait/touristapp/user/AppHomeActivity.java
+++ project/Athlone_Tourist_App/app/src/main/java/ie/ait/touristapp/user/AppHomeActivity.java
@@ -8,12 +8,81 @@ import android.content.Intent;
 import android.widget.Button;
 
 import ie.ait.touristapp.HelloAndroidActivity;
+import ie.ait.touristapp.Music_Activity;
 import ie.ait.touristapp.R;
 
 /**
  * Created by Collins on 12/11/2015.
+ * Modified by brendan on 19/11/2015 - added methods for buttons.
  */
 public class AppHomeActivity extends Activity{
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
+
+    public void onButtonClick(View v)//opens the Accomodation screen
+    {
+        if(v.getId()==R.id.Baccom)
+        {
+            Intent i = new Intent(this, LoginActivity.class);
+            startActivity(i);
+        }
+    }
+
+    public void onButtonClick2(View v)//opens the Music screen
+    {
+        if(v.getId()==R.id.Bmusic)
+        {
+            Intent i = new Intent(this, Music_Activity.class);
+            startActivity(i);
+        }
+    }
+
+    public void onButtonClick3(View v)//opens the Nightlife screen
+    {
+        if(v.getId()==R.id.Bnight)
+        {
+            Intent i = new Intent(this, LoginActivity.class);
+            startActivity(i);
+        }
+    }
+    public void onButtonClick4(View v)//opens the History Screen
+    {
+        if(v.getId()==R.id.Bhist)
+        {
+            Intent i = new Intent(this, LoginActivity.class);
+            startActivity(i);
+        }
+    }
+    public void onButtonClick5(View v)// opens the Activities Screen
+    {
+        if(v.getId()==R.id.Bactiv)
+        {
+            Intent i = new Intent(this, LoginActivity.class);
+            startActivity(i);
+        }
+    }
+
+    public void onButtonClick6(View v)//Opens the Food Screen
+    {
+        if(v.getId()==R.id.Bfood)
+        {
+            Intent i = new Intent(this, LoginActivity.class);
+            startActivity(i);
+        }
+    }
+    public void onButtonClick7(View v)//Opens the Login Screen, logs out the user
+    {
+        if(v.getId()==R.id.Blogout)
+        {
+            Intent i = new Intent(this, LoginActivity.class);
+            startActivity(i);
+        }
+    }
+
 }
+
 
---
+++ project/Athlone_Tourist_App/app/src/main/java/ie/ait/touristapp/user/Music_Activity.java
@@ -0,0 +1,14 @@
+package ie.ait.touristapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Music_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_);
+    }
+}
+
